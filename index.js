73c62e89dd2bccdd009b2135678d5f888a4ce5fe
Anny Caroline <annykozinski@gmail.com>
2026-08-13 15:56:56 -0300

    Add conversion from decimal to hexadecimal

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 
 const app = express();
@@ -6,20 +5,31 @@ const port = 3000;
 
 // Endpoint para converter decimal para binário
 app.get("/to-binary/:decimal", (req, res) => {
-  // Converte o parâmetro para número inteiro
   const decimal = parseInt(req.params.decimal, 10);
 
-  // Se não for número decimal retorna um erro com status 400
   if (isNaN(decimal)) {
     return res.status(400).json({ error: "Invalid decimal number" });
   }
 
   const binary = decimal.toString(2);
 
-  // Retorna a resposta como JSON
   res.json({ decimal, binary });
 });
 
+// Endpoint para converter decimal para hexadecimal
+app.get("/to-hex/:decimal", (req, res) => {
+  const decimal = parseInt(req.params.decimal, 10);
+
+  if (isNaN(decimal)) {
+    return res.status(400).json({ error: "Invalid decimal number" });
+  }
+
+  const hex = decimal.toString(16).toUpperCase();
+
+  res.json({ decimal, hex });
+});
+
+// Inicia o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
